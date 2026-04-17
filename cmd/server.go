13f2026358f349e0e8f2f8bf5aa19ba2560a959f
Jeremy Fergason <jeremy@fergason.me>
2026-04-16 22:39:24 -0700

    add `pvapi server` Cobra subcommand
    Runs the Fiber v3 app, reads port/allow_origins from viper, and handles
    SIGINT/SIGTERM via signal.NotifyContext + app.Shutdown. Returns non-zero
    on listen failure.

---
+++ cmd/server.go
@@ -0,0 +1,78 @@
+// Copyright 2021-2026
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+
+	"github.com/penny-vault/pv-api/api"
+)
+
+func init() {
+	rootCmd.AddCommand(serverCmd)
+
+	serverCmd.Flags().Int("server-port", 3000, "port to bind the HTTP server to")
+	serverCmd.Flags().String("server-allow-origins", "http://localhost:9000", "single CORS origin to allow; empty disables CORS")
+	bindPFlagsToViper(serverCmd)
+}
+
+var serverCmd = &cobra.Command{
+	Use:   "server",
+	Short: "Run the pvapi HTTP server",
+	RunE: func(_ *cobra.Command, _ []string) error {
+		ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+		defer cancel()
+
+		app := api.NewApp(api.Config{
+			Port:         conf.Server.Port,
+			AllowOrigins: conf.Server.AllowOrigins,
+		})
+
+		errCh := make(chan error, 1)
+		addr := fmt.Sprintf(":%d", conf.Server.Port)
+
+		go func() {
+			log.Info().Str("addr", addr).Msg("server listening")
+			if err := app.Listen(addr); err != nil && !errors.Is(err, http.ErrServerClosed) {
+				errCh <- err
+			}
+			close(errCh)
+		}()
+
+		select {
+		case err := <-errCh:
+			if err != nil {
+				return err
+			}
+			return nil
+		case <-ctx.Done():
+			log.Info().Msg("shutdown signal received")
+			if err := app.Shutdown(); err != nil {
+				return fmt.Errorf("fiber shutdown: %w", err)
+			}
+			return nil
+		}
+	},
+}
