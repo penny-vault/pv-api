70f23883470ea8b8e3fce1e55e878e2e8e9fd47e
Jeremy Fergason <jeremy@fergason.me>
2026-04-25 18:15:34 -0700

    refactor(portfolio): drop misleading json tag on internal CreateRequest

--- portfolio/types.go
+++ portfolio/types.go
@@ -65,7 +65,7 @@ type CreateRequest struct {
 	Benchmark        string
 	StartDate        *time.Time
 	EndDate          *time.Time
-	RunRetention     *int `json:"run_retention"`
+	RunRetention     *int
 }
 
 // UpdateRequest is what PATCH /portfolios/{slug} passes to the domain layer.
